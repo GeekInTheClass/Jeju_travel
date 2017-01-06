3215e87f9e48a063eb1f178a05c1b2fa9570cea7
purification <cscoi022@cscoi022ui-iMac.local>
2017-01-06 15:37:46 +0900

    place
    place

---
+++ place 오후 3.34.38/place/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  place
+//
+//  Created by cscoi022 on 2017. 1. 6..
+//  Copyright © 2017년 허정화. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
